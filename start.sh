68dfcca7150570e955e3b67172891a1ac5907f48
Per Mårtensson <pm@yodiwo.com>
2019-01-08 22:57:17 +0100

    seven

--- start.sh
+++ start.sh
@@ -10,7 +10,8 @@ sleep 1
 echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 2
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
+sleep 0
+#echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
+
 
 ./lora_pkt_fwd
