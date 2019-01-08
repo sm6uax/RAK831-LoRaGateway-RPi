34fcc429921a994181fecf7f9a3cff066700aa1f
Per Mårtensson <pm@yodiwo.com>
2019-01-08 22:48:51 +0100

    six

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ SX1301_RESET_BCM_PIN=40
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
+sleep 1
 echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
+sleep 2
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
